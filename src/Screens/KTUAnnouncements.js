81fb62586212a8d8e5ee2c6aa5612441a9476712
Rohit <rohitmpaul@gmail.com>
2020-01-14 01:23:01 +0530

    Move parsing and the logic into componentDidMount

--- src/Screens/KTUAnnouncements.js
+++ src/Screens/KTUAnnouncements.js
@@ -16,41 +16,29 @@ var HTMLParser = require("fast-html-parser");
 export class KTUAnnouncements extends Component {
   state = {
     loading: true,
-    root: {},
     rows: []
   };
 
   componentDidMount() {
+    var self = this;
     axios
       .get("https://ktu.edu.in/home.htm")
-      .then(data =>
-        this.setState({
-          root: HTMLParser.parse(data.data),
-          loading: false
-        })
-      )
+      .then(function(data) {
+        var x = HTMLParser.parse(data.data).querySelectorAll(".annuncement");
+        var str = JSON.stringify(x[0].rawText);
+        str = str.replace(/\\t/g, "");
+        str = str.replace(/"/g, "");
+        str = str.replace(/ {2}/g, ""); //means 2 spaces {2} is eslint requirement
+        x = str.split("\\n");
+        for (var i = 0; i < x.length; i++)
+          if (x[i].length < 2) {
+            x.splice(i, 1);
+            i = -1; //Every time splice is used a new array is copied into the old one, if 0 used 1st null don't
+          } // get deleted. If the statement not used then then index of the old array is used.
+
+        self.setState({ rows: x, loading: false });
+      })
       .catch(err => Alert.alert(err.message));
-  }
-
-  componentDidUpdate() {
-    if (!this.state.loading && !this.state.rows.length) {
-      var x = this.state.root.querySelectorAll(".annuncement");
-      console.log(x);
-      var str = "";
-      str = JSON.stringify(x[0].rawText);
-      str = str.replace(/\\t/g, "");
-      str = str.replace(/"/g, "");
-      str = str.replace(/ {2}/g, ""); //means 2 spaces {2} is eslint requirement
-      x = str.split("\\n");
-      for (var i = 0; i < x.length; i++)
-        if (x[i].length < 2) {
-          x.splice(i, 1);
-          i = -1; //Every time splice is used a new array is copied into the old one, if 0 used 1st null don't
-        } // get deleted. If the statement not used then then index of the old array is used.
-
-      // eslint-disable-next-line react/no-did-update-set-state
-      this.setState({ rows: x });
-    }
   }
 
   render() {
