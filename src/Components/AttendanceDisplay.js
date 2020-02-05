962864c02edae8abba308b4e2f2bd580640e138f
Rohit <rohitmpaul@gmail.com>
2020-02-05 23:45:54 +0530

    improve ui

--- src/Components/AttendanceDisplay.js
+++ src/Components/AttendanceDisplay.js
@@ -1,28 +1,28 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 function AttendanceDisplay(props) {
   return (
-    <Text
-      style={[styles.rows, props.percentage >= 75 ? styles.bhigh : styles.blow]}
-    >
-      {props.subject}
-      {"\n"}
-      Percentage:{" "}
-      <Text style={props.percentage >= 75 ? styles.high : styles.low}>
-        {props.percentage}
-        {"\t\t\t\t\t\t\t\t"}
-      </Text>
-      Entries till: {props.entriestill}
-    </Text>
+    <View style={props.percentage >= 75 ? styles.bhigh : styles.blow}>
+      <View>
+        <Text style={styles.rows}>{props.subject}</Text>
+      </View>
+      <View>
+        <Text style={styles.rows}>
+          Percentage:{" "}
+          <Text style={props.percentage >= 75 ? styles.high : styles.low}>
+            {props.percentage}
+          </Text>
+        </Text>
+        <Text style={styles.et}>Entries till: {props.entriestill}</Text>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   rows: {
     color: "white",
-    marginBottom: 15,
-    borderBottomWidth: 1,
     fontSize: 14.7
   },
   low: {
@@ -32,10 +32,17 @@ const styles = StyleSheet.create({
     color: "#8bc34a"
   },
   bhigh: {
-    borderColor: "#8bc34a"
+    borderColor: "#8bc34a",
+    borderBottomWidth: 1
   },
   blow: {
-    borderColor: "#ef5350"
+    borderColor: "#ef5350",
+    borderBottomWidth: 1
+  },
+  et: {
+    textAlign: "right",
+    color: "white",
+    fontSize: 12
   }
 });
 
